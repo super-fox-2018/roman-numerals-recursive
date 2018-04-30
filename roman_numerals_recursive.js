706f06b742b82f7232eb90e9e78bae915e7bc826
mario <panggihmario@gmail.com>
2018-04-30 20:17:48 +0700

    recrusif-roman

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,29 @@
 function toRoman(input) {
   // start your code here
-  return to_roman(input);
+
+  var rumus = {'M' : 1000,
+               'D' : 500,
+               'CD': 400,
+               'C' : 100,
+               'L' : 50,
+               'LX' :40,
+               'X' : 10,
+               'IX' : 9,
+               'V' : 5,
+               'IV' : 4,  
+               'I' : 1      
+              }
+if(input === 0){
+  return ''
+}
+else{
+  for(var i in rumus){
+    while(input >= rumus[i]){
+      return i + toRoman(input -rumus[i])
+    }
+  }
+  
+}
 }
 
 console.log('My totally sweet testing script for new roman\n')
