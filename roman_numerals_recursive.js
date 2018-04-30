944ce9a5a3120d994fb5da05d74e7b0501ee3c6b
Rin Ng <Rin08.ng@gmail.com>
2018-04-30 18:16:23 +0700

    Added code for roman numerals recursive

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,15 +1,47 @@
-function toRoman(input) {
-  // start your code here
-  return to_roman(input);
+function to_roman (num) {
+  var roman = ["I","IV","V","IX","X", "XL", "L", "C", "CD", "D", "CM", "M"];
+  var number = [1, 4, 5, 9, 10, 40, 50, 100, 400, 500, 900, 1000];
+  var arr = [];
+
+
+
+if(num === 0){
+  arr =  arr.join("");
+  return arr;
+}
+else{
+  for( var i = 0; i < number.length; i++){
+    if(num === number[i]){
+      arr.push(roman[i]);
+      num -= number[i];
+      arr =  arr.join("");
+      return (arr + to_roman(num));
+    }
+
+    if((num - number[i-1]) > 0 && (num - number[i]) < 0){
+      arr.push(roman[i-1]);
+      num -= number[i-1];
+      arr =  arr.join("");
+      return (arr + to_roman(num));
+
+      }
+
+    if((num - number[number.length -1]) > 0){
+      arr.push(roman[number.length - 1]);
+      num -= number[number.length - 1];
+      arr =  arr.join("");
+      return (arr + to_roman(num));
+     }
+    }
+  }
 }
 
+// Drive code
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
-console.log('——————|——————————|————————')
-console.log('4     | IV       | ', toRoman(4))
-console.log('9     | IX       | ', toRoman(9))
-console.log('23    | XXIII    | ', toRoman(23))
-console.log('1453  | MCDLIII  | ', toRoman(1453))
-console.log('1646  | MDCXLVI  | ', toRoman(1646))
-
-module.exports = toRoman
+console.log('——————|——————————|———————')
+console.log('4     | IV       | ', to_roman(4))
+console.log('9     | IX       | ', to_roman(9))
+console.log('13    | XIII     | ', to_roman(13))
+console.log('1453  | MCDLIII  | ', to_roman(1453))
+console.log('1646  | MDCXLVI  | ', to_roman(1646))
