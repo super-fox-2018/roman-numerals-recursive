d6db2e9ced798371bfc30a1c6f2bb91388df7050
handipriyono <handimacbook@gmail.com>
2018-04-30 19:23:29 +0700

    add roman num recursive

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,15 +1,45 @@
-function toRoman(input) {
-  // start your code here
-  return to_roman(input);
+function to_roman(num) {
+  if (num === 0) {
+    return '';
+  }
+
+  if (num >= 1000) {
+    return 'M' + to_roman(num - 1000);
+  } else if (num >= 900) {
+    return 'CM' + to_roman(num - 900);
+  } else if (num >= 500) {
+    return 'D' + to_roman(num - 500);
+  } else if (num >= 400) {
+    return 'CD' + to_roman(num - 400);
+  } else if (num >= 100) {
+    return 'C' + to_roman(num - 100);
+  } else if (num >= 90) {
+    return 'XC' + to_roman(num - 90);
+  } else if (num >= 50) {
+    return 'L' + to_roman(num - 50);
+  } else if (num >= 40) {
+    return 'XL' + to_roman(num - 40);
+  } else if (num >= 10) {
+    return 'X' + to_roman(num - 10);
+  } else if (num === 9) {
+    return 'IX';
+  } else if (num >= 5) {
+    return 'V' + to_roman(num - 5);
+  } else if (num === 4) {
+    return 'IV';
+  } else if (num < 4) {
+    return 'I' + to_roman(num - 1);
+  }
 }
 
-console.log('My totally sweet testing script for new roman\n')
-console.log('input | expected | actual')
-console.log('——————|——————————|————————')
-console.log('4     | IV       | ', toRoman(4))
-console.log('9     | IX       | ', toRoman(9))
-console.log('23    | XXIII    | ', toRoman(23))
-console.log('1453  | MCDLIII  | ', toRoman(1453))
-console.log('1646  | MDCXLVI  | ', toRoman(1646))
-
-module.exports = toRoman
+// Drive code
+console.log('My totally sweet testing script for new roman\n');
+console.log('input | expected | actual');
+console.log('——————|——————————|———————');
+console.log('4     | IV       | ', to_roman(4));
+console.log('9     | IX       | ', to_roman(9));
+console.log('13    | XIII     | ', to_roman(13));
+console.log('1453  | MCDLIII  | ', to_roman(1453));
+console.log('1646  | MDCXLVI  | ', to_roman(1646));
+console.log(to_roman(48));
+console.log(to_roman(389));
