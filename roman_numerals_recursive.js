8ab13844e474b1d1683cfe427b21e97f24f3ac1c
Rhesa Utomo <venderblitz@gmail.com>
2018-04-30 23:50:24 +0700

    first commit

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,35 @@
 function toRoman(input) {
   // start your code here
-  return to_roman(input);
+  if (input === 0) {
+    return '';
+  }
+
+  var romanKeys = [
+    {key: 'M', val: 1000},
+    {key: 'CM', val: 900},
+    {key: 'D', val: 500},
+    {key: 'CD', val: 400},
+    {key: 'C', val: 100},
+    {key: 'XC', val: 90},
+    {key: 'L', val: 50},
+    {key: 'XL', val: 40},
+    {key: 'X', val: 10},
+    {key: 'IX', val: 9},
+    {key: 'V', val: 5},
+    {key: 'IV', val: 4},
+    {key: 'I', val: 1}
+  ];
+
+  var display = '';
+
+  for (var i = 0; i < romanKeys.length; i++) {
+    if (input >= romanKeys[i].val) {
+      display = display + romanKeys[i].key;
+      input = input - romanKeys[i].val
+    }
+  }
+  
+  return display + toRoman(input);
 }
 
 console.log('My totally sweet testing script for new roman\n')
