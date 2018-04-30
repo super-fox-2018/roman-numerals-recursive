a8441ca3b895807629fd322bcbf8f9a5701a26be
bramaprasetyopermana <bramprasetyop@gmail.com>
2018-04-30 15:48:11 +0700

    brama-prasetyo

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,71 @@
 function toRoman(input) {
-  // start your code here
-  return to_roman(input);
+  var resultRomanNumeral=[]
+  var romanNumeral=[
+    {
+      number:1000,
+      roman:'M'
+    },
+    {
+      number:900,
+      roman:'CM'
+    },
+    {
+      number:500,
+      roman:'D'
+    },
+    {
+      number:400,
+      roman:'CD'
+    },
+    {
+      number:100,
+      roman:'C'
+    },
+    {
+      number:90,
+      roman:'XC'
+    },
+    {
+      number:50,
+      roman:'L'
+    },
+    {
+      number:40,
+      roman:'XL'
+    },
+    {
+      number:10,
+      roman:'X'
+    },
+    {
+      number:9,
+      roman:'IX'
+    },
+    {
+      number:5,
+      roman:'V'
+    },
+    {
+      number:4,
+      roman:'IV'
+    },
+    {
+      number:1,
+      roman:'I'
+    }
+  ]
+
+if (input === 0) {
+  return '';
+}
+else {
+  for (var i = 0; i < romanNumeral.length; i++) {
+    while (input >= romanNumeral[i].number) {
+      input = input - romanNumeral[i].number
+      return romanNumeral[i].roman + toRoman(input);
+    }
+  }
+}
 }
 
 console.log('My totally sweet testing script for new roman\n')
@@ -12,4 +77,4 @@ console.log('23    | XXIII    | ', toRoman(23))
 console.log('1453  | MCDLIII  | ', toRoman(1453))
 console.log('1646  | MDCXLVI  | ', toRoman(1646))
 
-module.exports = toRoman
+// module.exports = toRoman
