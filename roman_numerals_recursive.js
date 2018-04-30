4cc45b47809efc0b9cfb9914bef94223a3429feb
alprak <you@example.com>
2018-04-30 17:48:22 +0700

    done

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,22 @@
 function toRoman(input) {
   // start your code here
-  return to_roman(input);
+  var roman=['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+  var angka=[1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  
+  
+  if(input==0) {
+  	return ""
+  }else{
+  	for(var i=0;i<angka.length;i++) {
+ 		while(input>=angka[i]) {
+ 			input=input-angka[i]
+ 			return roman[i]+toRoman(input)
+ 		}
+ 	}
+  }
+
+
+ // return to_roman(input);
 }
 
 console.log('My totally sweet testing script for new roman\n')
