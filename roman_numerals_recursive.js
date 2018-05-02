d8e5406bfa7608be820cab533053c893f263fcc1
handipriyono <handimacbook@gmail.com>
2018-05-02 22:22:43 +0700

    refactor recursive roman numerals

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,35 +1,32 @@
 function to_roman(num) {
-  if (num === 0) {
-    return '';
+  var obj = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  var ad = '';
+
+  for (let i in obj) {
+    if (num >= obj[i]) {
+      ad += i;
+
+      num -= obj[i];
+      return ad + to_roman(num);
+    }
   }
 
-  if (num >= 1000) {
-    return 'M' + to_roman(num - 1000);
-  } else if (num >= 900) {
-    return 'CM' + to_roman(num - 900);
-  } else if (num >= 500) {
-    return 'D' + to_roman(num - 500);
-  } else if (num >= 400) {
-    return 'CD' + to_roman(num - 400);
-  } else if (num >= 100) {
-    return 'C' + to_roman(num - 100);
-  } else if (num >= 90) {
-    return 'XC' + to_roman(num - 90);
-  } else if (num >= 50) {
-    return 'L' + to_roman(num - 50);
-  } else if (num >= 40) {
-    return 'XL' + to_roman(num - 40);
-  } else if (num >= 10) {
-    return 'X' + to_roman(num - 10);
-  } else if (num === 9) {
-    return 'IX';
-  } else if (num >= 5) {
-    return 'V' + to_roman(num - 5);
-  } else if (num === 4) {
-    return 'IV';
-  } else if (num < 4) {
-    return 'I' + to_roman(num - 1);
-  }
+  return ad;
 }
 
 // Drive code
@@ -41,5 +38,5 @@ console.log('9     | IX       | ', to_roman(9));
 console.log('13    | XIII     | ', to_roman(13));
 console.log('1453  | MCDLIII  | ', to_roman(1453));
 console.log('1646  | MDCXLVI  | ', to_roman(1646));
-console.log(to_roman(48));
-console.log(to_roman(389));
+console.log(to_roman(4));
+console.log(to_roman(1222));
